395e7d23761151bd00965562d3b780e553910d3a
rosalinda <abreuparrarosalinda@gmail.com>
2024-11-20 19:06:15 -0400

    user validations

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -15,13 +15,20 @@ async function login(req, res) {
       return res.status(400).json({ error: 'Invalid username or password' });
     }
 
+    // Verificar si la cuenta está eliminada
+    if (user.deletedAt) {
+      return res.status(403).json({ error: 'Account has been deleted' });
+    }
+
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
       return res.status(400).json({ error: 'Invalid username or password' });
     }
 
-    req.session.user = { id: user._id, access: user.access }; // Guardar el ID del usuario y el tipo de acceso en la sesión
+    req.session.user = user._id;
+    req.session.access = user.access;
+
     res.status(200).json({ message: 'Login successful', access: user.access });
   } catch (error) {
     console.error('Login error:', error);
